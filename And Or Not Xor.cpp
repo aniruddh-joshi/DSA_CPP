67a71b52a7d56fcfb76b4fcecf6d92a6541b6d32
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2023-02-22 18:34:04 +0530

    And Or Not Xor.cpp

---
+++ And Or Not Xor.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout<<"Input a\n";
+    cin>>a;
+    cout<<"Input b\n";
+    cin>>b;
+    
+    cout<<"a&b is:- "<<(a&b)<<endl;
+    cout<<"a|b is:- "<<(a|b)<<endl;
+    cout<<"~a is:- "<<(~a)<<endl;
+    cout<<"a^b is:- "<<(a^b)<<endl;
+}
