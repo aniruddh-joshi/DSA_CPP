cf101a58a52b788d44172042344695b4b2b59918
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2023-02-23 19:04:04 +0530

    Decimal to Binary without Array

---
+++ Decimal to Binary.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the Decimal Number:- ";
+    cin>>n;
+    int ans=0;
+    int i=0;
+    while(n!=0){
+        int bit=n&1;
+        ans=(bit*pow(10,i))+ans;
+        n=n>>1;
+        i++;
+    }
+    cout<<"Binary Conversion is:- "<<ans<<endl;
+}
