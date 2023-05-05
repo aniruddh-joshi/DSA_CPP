b0739bfa566c40cfc405f442a68f35e1f3b77dc6
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2023-05-05 10:06:46 +0000

    Complete Deletion - Linked List

---
+++ Linked_List_Deletion.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node*next;
+};
+
+void insertionHead(Node* &head,int data){
+    Node*temp=new Node();
+    temp->data=data;
+    temp->next=NULL;
+    temp->next=head;
+    head=temp;
+}
+
+void insertionTail(Node* &tail,int data){
+    Node*temp=new Node();
+    temp->data=data;
+    temp->next=NULL;
+    tail->next=temp;
+    tail=temp;
+}
+
+void insertionMid(Node* &head,Node* &tail,int position,int data){
+    //insertion at start
+    if(position==1){
+        insertionHead(head,data);
+        return;
+    }
+
+    //insertion at specific position
+    Node*temp=head;
+    int cnt=1;
+    while(cnt < position-1){
+        temp=temp->next;
+        cnt++;
+    }
+    Node* nodeToinsert=new Node();
+    nodeToinsert->data=data;
+    nodeToinsert->next=NULL;
+    nodeToinsert->next=temp->next;
+    temp->next=nodeToinsert;
+
+    //insertion at last
+    if(temp->next==NULL){
+        insertionTail(tail,data);
+        return;
+    }
+}
+
+void deletion(Node*head,int position){
+    //deletion first node
+    if(position==1){
+        Node*temp=head;
+        head=head->next;
+        temp->next=NULL;
+        delete temp;
+    }
+    else{
+        Node*current=head;
+        Node*prev=NULL;
+        int cnt=1;
+        while(cnt < position){
+            prev=current;
+            current=current->next;
+            cnt++;
+        }
+        prev->next=current->next;
+        current->next=NULL;
+        delete current;
+    }
+}
+
+void print(Node* &head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    Node*node1=new Node();
+    node1->data=10;
+    node1->next=NULL;
+
+    Node*head=node1;
+    Node*tail=node1;
+    print(head);
+
+    insertionTail(tail,20);
+    print(head);
+
+    insertionTail(tail,30);
+    print(head);
+
+    insertionMid(head,tail,4,22);
+    print(head);
+
+    deletion(head,2);
+    print(head);
+    
+    return 0;
+}
